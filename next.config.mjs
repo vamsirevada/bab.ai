0b77571ff2576ee7572b4f59a58923ad0ac3096a
Revada Vamsi <38239734+vamsirevada@users.noreply.github.com>
2025-08-08 06:22:17 +0530

    db connection fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
+  env: {
+    DB_URL: process.env.DB_URL,
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+  },
 }
 export default nextConfig
